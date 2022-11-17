bd288edcd027fc2c9e9d33fb2428b1aad0bd9d18
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-17 07:05:09 +0530

    appended text using appendFileSync method

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,11 @@
 const fs = require("fs");
-fs.writeFileSync("notes.txt", "My name is Sree Lakshmi");
+const { text } = require("stream/consumers");
+fs.writeFileSync("notes.txt", "My name is Sree Lakshmi. ");
+
+// Challenge: Append a message to notes.txt
+
+// 1. Use appendFileSync() to append to the file
+// 2. Run the script
+// 3. Check your work by opening the file and viewing the appended text.
+
+fs.appendFileSync("notes.txt", `I'm a front-end developer.`);
