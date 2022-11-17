1b939c0a204971b27e036cc007ff05d2fe16c040
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-17 07:07:15 +0530

    console name to log

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,14 @@
-const fs = require("fs");
-const { text } = require("stream/consumers");
-fs.writeFileSync("notes.txt", "My name is Sree Lakshmi. ");
+// const fs = require("fs");
+// const { text } = require("stream/consumers");
+// fs.writeFileSync("notes.txt", "My name is Sree Lakshmi. ");
 
-// Challenge: Append a message to notes.txt
+// // Challenge: Append a message to notes.txt
 
-// 1. Use appendFileSync() to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text.
+// // 1. Use appendFileSync() to append to the file
+// // 2. Run the script
+// // 3. Check your work by opening the file and viewing the appended text.
 
-fs.appendFileSync("notes.txt", `I'm a front-end developer.`);
+// fs.appendFileSync("notes.txt", `I'm a front-end developer.`);
+
+const name = "Andrew";
+console.log(name);
