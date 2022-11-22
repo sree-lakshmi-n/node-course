1432f5fd0f683706a5bbf9d5a1ab6af9c02183a2
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-22 17:48:33 +0530

    Customise yargs version. Create add command.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,18 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
 const getNotes = require("./notes");
+
+// Customise yargs version
+yargs.version("1.1.0");
+
+// add, remove, read, list
+
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding new note");
+  },
+});
 console.log(yargs.argv);
